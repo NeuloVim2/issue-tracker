fad6849cef33a70fb353e866a40fd98781e1dc71
Petro Kirushkin <pkyriushkin@nymbus.com>
2021-11-21 01:53:14 +0200

    created 2 functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -44,10 +44,51 @@ suite('Functional Tests', function () {
 
         })
         it('Create an issue with only required fields', () => {
+            let requestData = {
+                issue_title: "test1",
+                issue_text: "test1 text",
+                created_by: "testUser",
+            }
 
+            chai
+                .request(server)
+                .post('/api/issues/:project')
+                .type('form')
+                .send(requestData)
+                .end((err, res) => {
+                    if (err)
+                        console.log(err);
+
+                    let date = new Date().toISOString();
+             
+
+                    assert.equal(res.status, 200);
+                    assert.isNotEmpty(res.body._id, "_id - is empty");
+                    assert.equal(res.body.issue_title, requestData.issue_title, "issue_title - invalid");
+                    assert.equal(res.body.issue_text, requestData.issue_text, "issue_text - invalid");
+                    assert.operator(res.body.created_on, '<=', date, 'created_on is invalid');
+                    assert.operator(res.body.updated_on, '<=', date, 'created_on is invalid');
+                    assert.equal(res.body.created_by,  requestData.created_by, "created_by - invalid");
+                    assert.equal(res.body.assigned_to, "", "assigned_to - invalid");
+                    assert.isTrue(res.body.open, 'open is not true');
+                    assert.equal(res.body.status_text, "", "status_text - invalid");
+                    done();
+                });
         })
-        it('Create an issue with missing required fields', () => {
-
+        it('Create an issue with missing required fields', (done) => {
+            chai
+                .request(server)
+                .post('/api/issues/:project')
+                .type('form')
+                .send(requestData)
+                .end((err, res) => {
+                    if(err)
+                      console.log(err);
+                
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, "required field(s) missing", 'returned value is valid')
+                    done();
+                })
         })
     })
 });
