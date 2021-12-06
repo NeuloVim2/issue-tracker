de5220707ee348791b532f944daf5065035b444e
Petro Kirushkin <pkyriushkin@nymbus.com>
2021-12-06 23:26:26 +0200

    added Schema fields to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,28 @@ module.exports = {
     'class-methods-use-this': 'off',
     'no-underscore-dangle': 'off',
     'no-shadow': ['error', { allow: ['done', 'err', 'res', 'doc'] }],
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      {
+        version: '>=8.3.0',
+        ignores: ['spreadElements'],
+      },
+    ],
+    camelcase: [
+      'error',
+      {
+        properties: 'never',
+        ignoreDestructuring: true,
+        allow: [
+          'issue_title',
+          'issue_text',
+          'created_on',
+          'updated_on',
+          'created_by',
+          'assigned_to',
+          'status_text',
+        ],
+      },
+    ],
   },
 };
--- routes/api.js
+++ routes/api.js
@@ -1,57 +1,64 @@
-'use strict';
-
 const Issue = require('../db/modeles/Issue');
 
+const verifyUrlQuery = (query) => {
+  const issueSchema = [
+    '_id',
+    'issue_title',
+    'issue_text',
+    'created_on',
+    'updated_on',
+    'created_by',
+    'assigned_to',
+    'open',
+    'status_text',
+  ];
+  return query.every((param) => issueSchema.includes(param));
+};
+
 module.exports = function (app) {
+  app
+    .route('/api/issues/:project')
 
-  app.route('/api/issues/:project')
-
-    .get(function (req, res) {
-      console.log("GET request to ", req.originalUrl);
-      let project = req.params.project;
-      let queryKeys = Object.keys(req.query);
+    .get((req, res) => {
+      console.log('GET request to ', req.originalUrl);
+      const { project } = req.params;
+      const queryKeys = Object.keys(req.query);
       console.log(queryKeys);
 
       // Check if there are no params in url(/api/issues/apitest?open=true)
       if (queryKeys.length === 0) {
-        Issue.find({ project: project },)
+        Issue.find({ project: project })
           .select('-__v -project')
           .exec((err, issues) => {
-            if (err)
-              console.log("unable to get issue from db;\n", err);
+            if (err) console.log('unable to get issue from db;\n', err);
 
             console.log(issues);
             res.json(issues);
-          })
+          });
+        // Check if query contains only Issue schema properties
+      } else if (verifyUrlQuery(queryKeys)) {
+        const issueToSearch = {
+          ...req.query,
+          project: project,
+        };
+        Issue.find(issueToSearch)
+          .select('-__v -project')
+          .exec((err, issues) => {
+            if (err) console.log('unable to get issue from db;\n', err);
+
+            console.log(issues);
+            res.json(issues);
+          });
       } else {
-
-        //Check if query contains only Issue schema properties
-        if (verifyUrlQuery(queryKeys)) {
-          let issueToSearch = {
-            ...req.query,
-            project: project
-          };
-          Issue.find(issueToSearch)
-            .select('-__v -project')
-            .exec((err, issues) => {
-              if (err)
-                console.log("unable to get issue from db;\n", err);
-
-              console.log(issues);
-              res.json(issues);
-            })
-        } else {
-          res.status(404)
-             .json({error: "invalid query in url"});
-        }
+        res.status(404).json({ error: 'invalid query in url' });
       }
     })
 
-    .post(function (req, res) {
-      let project = req.params.project;
-      let issueToCreate = {
+    .post((req, res) => {
+      const { project } = req.params;
+      const issueToCreate = {
         ...req.body,
-        project: project
+        project: project,
       };
 
       Issue.create(issueToCreate, (err, issue) => {
@@ -59,7 +66,8 @@ module.exports = function (app) {
           console.log(`unable to create new issue. \n ${err}`);
           res.json({ error: 'required field(s) missing' });
         } else {
-          let { _id,
+          const {
+            _id,
             issue_title,
             issue_text,
             created_on,
@@ -67,7 +75,8 @@ module.exports = function (app) {
             created_by,
             assigned_to,
             open,
-            status_text } = issue;
+            status_text,
+          } = issue;
 
           res.json({
             _id: _id,
@@ -78,50 +87,32 @@ module.exports = function (app) {
             created_by: created_by,
             assigned_to: assigned_to,
             open: open,
-            status_text: status_text
+            status_text: status_text,
           });
         }
-      })
+      });
     })
 
-    .put(function (req, res) {
-      console.log('make PUT request')
-      if(req.body._id){
-        console.log('id is true')
+    .put((req, res) => {
+      console.log('make PUT request');
+      if (req.body._id) {
+        console.log('id is true');
         console.log(Object.entries(req.body).length > 0);
-        if(Object.entries(req.body).length > 0){
+        if (Object.entries(req.body).length > 0) {
           Issue.findByIdAndUpdate(req.body._id, req.body, (err, issue) => {
-            if(err)
-              console.log(err);
-    
-            res.json({result: 'successfully updated', _id: issue._id})
-          })
+            if (err) console.log(err);
+
+            res.json({ result: 'successfully updated', _id: issue._id });
+          });
         } else {
-          res.json( { error: 'no update field(s) sent', '_id': req.body._id });
+          res.json({ error: 'no update field(s) sent', _id: req.body._id });
         }
       } else {
-        res.json({ error: 'missing _id' })
+        res.json({ error: 'missing _id' });
       }
     })
 
-    .delete(function (req, res) {
-      let project = req.params.project;
-
+    .delete((req, res) => {
+      const { project } = req.params;
     });
-
 };
-
-const verifyUrlQuery = (query) => {
-  const issueSchema = [
-    "_id",
-    "issue_title",
-    "issue_text",
-    "created_on",
-    "updated_on",
-    "created_by",
-    "assigned_to",
-    "open",
-    "status_text"
-  ]
-  return query.every((param) => issueSchema.includes(param))
-}
