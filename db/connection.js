f2be8a88e4fd2a5f67cfc5092cdf4a8c0c64e1d0
Petro Kirushkin <pkyriushkin@nymbus.com>
2021-12-08 12:28:50 +0200

    formated connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGO_URI;
 
 // Connect mongoose to MongoDB database
-mongoose.connect(uri ,{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'cnnection error:'));
 db.once('open', () => {
   console.log('connection to db - successful');
-})
+});
 
 module.exports = db;
